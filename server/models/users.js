5cdbb3590c28260a3f1427a78ce774fde4855759
Erlinis Quintana <erlinis.quintana@gmail.com>
2018-12-10 19:52:45 -0500

    Creates user schema for moongse

---
+++ server/models/users.js
@@ -0,0 +1,38 @@
+// User model structure definition
+
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required']
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  role: {
+    type: String,
+    default: 'USER_ROLE',
+    required: true
+  },
+  active: {
+    type: Boolean,
+    default: true
+  },
+  google: {
+    type: Boolean
+    default: false
+  }
+});
+
+module.exports = moongose.models('User', userSchema);
